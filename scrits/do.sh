8cda8b4e3ce0192ab34ae3901656b96070b9668c
unisolzp <unisolzp@gmail.com>
2021-02-22 13:02:14 +0200

    Create do.sh

---
+++ scrits/do.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -L "https://raw.githubusercontent.com/unisolzp/linux_dependencies/main/scrits/node_install" -o /opt/install.sh 
+chmod +x /opt/install.sh
+sh /opt/install.sh preinstall
